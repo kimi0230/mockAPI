df07a78b87907bcff23958e27b79099093658b67
kimi <kimi0230@gmail.com>
2019-09-06 16:39:28 +0800

    test

---
+++ api/controllers/TestController.js
@@ -0,0 +1,14 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index: async function (req, res) {
+    return res.json({
+      name: "kimi"
+    });
+  }
+};
